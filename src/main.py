a74b3ebdaf302ee85081f21cb83f267a7be72aef
rateme5stars <dzungardan@gmail.com>
2023-07-12 12:29:52 +0200

    add argparse

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from converter import Converter
+import argparse
 
 
 def main():
-    input_file = './resources/file_example.tiff'
-    output_file = './resources/file_example.png'
-    converter = Converter(input_file, output_file)
-    converter.convert_pdf_to_png()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_file",
+                        help="The path to the input TIFF file.")
+    parser.add_argument("-o", "--output_file",
+                        help="The path to the output PNG file.")
+    args = parser.parse_args()
+
+    converter = Converter(args.input_file, args.output_file)
+    converter.convert_tiff_to_png()
 
 
 if __name__ == "__main__":
